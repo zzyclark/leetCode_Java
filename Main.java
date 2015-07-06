bd0570e6d095f07a7d3ba9e9ab772402a0cb69ab
zzyclark <zhangzongyangclark@gmail.com>
2015-07-06 17:59:58 +0800

    problem 5

--- Main.java
+++ Main.java
@@ -3,8 +3,9 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args)
     {
-	//System.out.println(Arrays.toString(TwoSum.twoSum(new int[] {3,2,4}, 6)));
-//	System.out.println(LengthOfLongestSubstring.alternateFunction("aab"));
-        System.out.println(FindMedianSortArrays.alternateFunction(new int[] {}, new int[] {1, 2, 3, 4}));
+//        System.out.println(Arrays.toString(TwoSum.twoSum(new int[] {3,2,4}, 6)));
+//        System.out.println(LengthOfLongestSubstring.alternateFunction("aab"));
+//        System.out.println(FindMedianSortArrays.alternateFunction(new int[] {}, new int[] {1, 2, 3, 4}));
+        System.out.println(LongestPalindromicSubstring.longestPalindrome("bb"));
     }
 }
