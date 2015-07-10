5b9146b853c4cdf3886077119b805bf7935a6040
zzyclark <zhangzongyangclark@gmail.com>
2015-07-10 11:15:37 +0800

    Problem 7 and 8

--- Main.java
+++ Main.java
@@ -6,7 +6,9 @@ public class Main {
 //        System.out.println(Arrays.toString(TwoSum.twoSum(new int[] {3,2,4}, 6)));
 //        System.out.println(LengthOfLongestSubstring.alternateFunction("aab"));
 //        System.out.println(FindMedianSortArrays.alternateFunction(new int[] {}, new int[] {1, 2, 3, 4}));
-        System.out.println(LongestPalindromicSubstring.longestPalindrome("bb"));
-	System.out.println(ZigzagConvertion.convert("PAYPALISHIRING", 3));
+//        System.out.println(LongestPalindromicSubstring.longestPalindrome("bb"));
+//        System.out.println(ZigzagConvertion.convert("PAYPALISHIRING", 3));
+//	     System.out.println(ReverseInteger.reverse(100));
+	    System.out.println(Atoi.myAtoi("1"));
     }
 }
