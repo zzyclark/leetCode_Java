307b80f3be0903e68d0ff268114d13e0efd8b6ca
zzyclark <zhangzongyangclark@gmail.com>
2015-11-17 10:29:11 +0800

    README update, restructure problem 30 solution. \n Change subString storage from map to normal array.

--- 30_Substring_with_Concatenation_of_All_Words/FindSubString.java
+++ 30_Substring_with_Concatenation_of_All_Words/FindSubString.java
@@ -5,68 +5,68 @@ import java.util.*;
  * @Date 16-Nov-2015
  */
 public class FindSubString {
-    private static Map<Integer, String> substringCache;
-
     public static List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> result = new ArrayList<Integer>();
         int wordLength = words[0].length();
         int stringLength = s.length();
-        Map<String, Integer> keyMap = constructKeyMap(words);
+
+        if (words.length == 0) {
+            return result;
+        }
+
+        //This check must have, otherwise one of the test case will throw TLE
+        if (words.length * wordLength > stringLength) {
+            return result;
+        }
+
+        String[] subStringArray = new String[stringLength - wordLength + 1];
+        Map<String, Integer> keyMap = new HashMap<String, Integer>();
         Map<String, Integer> testMap = new HashMap<String, Integer>();
-        List<Integer> result = new ArrayList<Integer>();
-        constructSubstringCache(s, wordLength);
 
-        OuterLoop:
+        //construct key map
+        for (String word : words) {
+            keyMap.put(word, keyMap.getOrDefault(word, 0) + 1);
+        }
+
+        //construct sub string array
+        for (int i = 0 ; i < subStringArray.length; ++i) {
+            String subStr = s.substring(i, i + wordLength);
+            subStringArray[i] = keyMap.containsKey(subStr) ? subStr : "";
+        }
+
         for (int i = 0; i < wordLength; ++i) {
-            testMap.clear();
+            int count = 0;
             int startPos = i;
-            int wordCount = 0;
-
-            while(startPos < stringLength - wordLength + 1) {
-                String subStr = substringCache.get(startPos);
-                if (keyMap.containsKey(subStr)) {
-                    if (testMap.containsKey(subStr)) {
-                        int currentOccurrence = testMap.get(subStr);
-                        //words in subString must occur exactly once
-                        if (currentOccurrence + 1 > keyMap.get(subStr)) {
-                            //find last occur of this string, start again from there, and no need to clear map, only minus or remove the occurrence before that word
-                            int removeStartPos = startPos - wordCount * wordLength;
-                            InnerLoop:
-                            while(true) {
-                                String oldWordCheck = substringCache.get(removeStartPos);
-
-                                if (testMap.get(oldWordCheck) > 1) {
-                                    testMap.put(oldWordCheck, testMap.get(oldWordCheck) - 1);
-                                } else {
-                                    testMap.remove(oldWordCheck);
-                                }
-                                removeStartPos += wordLength;
-                                wordCount--;
-
-                                if (subStr.equals(oldWordCheck)) {
-                                    break InnerLoop;
-                                }
-                            }
-                        } else {
-                            testMap.put(subStr, currentOccurrence + 1);
-                            wordCount++;
-                            startPos += wordLength;
-                        }
-                    } else {
-                        testMap.put(subStr, 1);
-                        wordCount++;
-                        startPos += wordLength;
-                    }
-
-                    if (wordCount == words.length) {
-                        wordCount = 0;
-                        testMap.clear();
-                        result.add(startPos - wordLength*words.length);
-                        startPos = startPos - wordLength*(words.length-1);
-                    }
-                } else {
+            testMap.clear();
+            for (int j = i; j < stringLength - wordLength + 1; j += wordLength) {
+                String subStr = subStringArray[j];
+                if (subStr.equals("")) {
                     testMap.clear();
-                    wordCount = 0;
-                    startPos+=wordLength;
+                    count = 0;
+                    startPos = j + wordLength;
+                    continue;
+                } else {
+                    testMap.put(subStr, testMap.getOrDefault(subStr, 0) + 1);
+                    count++;
+                }
+
+                //add new word, check over counting
+                if (testMap.get(subStr) > keyMap.get(subStr)) {
+                    while(true) {
+                        String removeCandidate = subStringArray[startPos];
+                        testMap.put(removeCandidate, testMap.get(removeCandidate) - 1);
+                        startPos += wordLength;
+                        count--;
+
+                        if (removeCandidate.equals(subStr)) {
+                            break;
+                        }
+                    }
+                }
+
+                //if count equal to the total word number of the words length, add the start pos to result
+                if (count == words.length) {
+                    result.add(startPos);
                 }
             }
         }
@@ -74,31 +74,9 @@ public class FindSubString {
         return result;
     }
 
-    private static Map<String, Integer> constructKeyMap(String[] words) {
-        Map<String, Integer> keyMap = new HashMap<String, Integer>();
-
-        for (String word : words) {
-            if (keyMap.containsKey(word)) {
-                keyMap.put(word, keyMap.get(word)+1);
-            } else {
-                keyMap.put(word, 1);
-            }
-        }
-
-        return keyMap;
-    }
-
-    private static void constructSubstringCache(String s, Integer wordLength) {
-        int stringLength = s.length();
-        substringCache = new HashMap<Integer, String>();
-        for (int i = 0; i < stringLength - wordLength + 1; ++i) {
-            substringCache.put(i, s.substring(i , i + wordLength));
-        }
-    }
-
     public static void main(String[] args) {
-        List<Integer> test = findSubstring("barfoofoobarthefoobarman"
-                , new String[] {"bar", "foo", "the"});
+        List<Integer> test = findSubstring("aaaaaaaa"
+                , new String[] {"aa", "aa", "aa"});
         for (Integer i : test) {
             System.out.print(i);
         }
